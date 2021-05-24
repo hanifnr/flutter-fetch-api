255ab83bb0094b1aa7de7982c9d711f02b6a68c8
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-05-24 18:02:50 +0700

    init commit

---
+++ lib/view/AlbumView.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:test2/controller/AlbumController.dart';
+import 'package:test2/model/Album.dart';
+import 'dart:async';
+
+class AlbumView extends StatefulWidget {
+  @override
+  _AlbumViewState createState() => _AlbumViewState();
+}
+
+class _AlbumViewState extends State<AlbumView> {
+  late Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Fetch JSON Demo'
+        ),
+      ),
+      body: Center(
+        child: FutureBuilder<Album>(
+          future: futureAlbum,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data!.title);
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+
+            return CircularProgressIndicator();
+          },
+        ),
+      ),
+    );
+  }
+}
